ebbf0c22587c3c876ba8f7e9bc76f942ecc0047a
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-03-22 13:51:16 +0200

    api alustettu

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const dotenv=require('dotenv');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(helmet());
+app.use(cors());
+dotenv.config();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
