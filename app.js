6ca7996292c2f3842bcdfeec003827e1b316e37c
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-03-25 09:59:14 +0200

    webtoken toimii

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var bookRouter = require('./routes/book');
 var userRouter = require('./routes/user');
 var loginRouter = require('./routes/login');
+const jwt = require('jsonwebtoken');
 
 var app = express();
 app.use(helmet());
@@ -22,9 +23,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//suojaamattomat reitit
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+
+app.use(authenticateToken);
+//suojatut reitit
 app.use('/book', bookRouter);
 app.use('/user', userRouter);
-app.use('/login', loginRouter);
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
+
 
 module.exports = app;
